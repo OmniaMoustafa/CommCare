2d215751572dcaa9479d3e9316a1b77ac8fce039
RanaAbdelaziz <ranaa7517@gmail.com>
2018-06-19 10:38:12 +0200

    DeletAppointment

--- src/app/appointment/appointment.module.ts
+++ src/app/appointment/appointment.module.ts
@@ -3,10 +3,14 @@ import { CommonModule } from '@angular/common';
 import { AppointmentComponent } from './appointment.component';
 import { AppointmentFormComponent } from './appointment-form/appointment-form.component';
 import { AcceptanceComponent } from './Acceptance/Acceptance.component';
+import { RouterModule, Routes } from '@angular/router';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forRoot([
+      {path:'acceptance', component:AcceptanceComponent}
+    ])
   ],
   declarations: [AppointmentComponent,
     AppointmentFormComponent,
